0ccc559f79d41ddd5675d7d5aaa6e85dbbf001c9
hasanthi <130127842+Hasanthi-Swarna@users.noreply.github.com>
2025-10-22 16:05:11 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:taskmanager/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Task Manager App test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(TaskManagerApp());
   });
